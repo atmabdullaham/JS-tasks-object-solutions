445fc0c600ab730dd4b69b41f99e5ac08c9db7d8
atmabdullaham <atmabdullaham@gmail.com>
2024-11-13 22:02:01 +0600

    added task 2, 3 and 4

---
+++ task_04.js
@@ -0,0 +1,14 @@
+// Task - 4
+// Count the number of properties.
+
+// Input:
+
+let student = {
+ name: 'Ariana Grande',
+ age: 21,
+ city: 'Gaibandha',
+ isStudent: true
+};
+
+const propertyCount = Object.keys(student).length;
+console.log(propertyCount); // Output: 4
